6b973dd7dfe94084b83410cc1880d6438aaf140f
NoZ915 <tina0915tw0915@gmail.com>
2024-07-13 09:08:51 +0800

    finished: ~L29

---
+++ BullkyWeb/BullkyWeb/Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using BullkyWeb.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace BullkyWeb.Data
+{
+    public class ApplicationDbContext: DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Category> Categories { get; set; }
+    }
+}
---
+++ BullkyWeb/BullkyWeb/Models/Category.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BullkyWeb.Models
+{
+    public class Category
+    {
+        [Key]
+        public int Category__Id { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        public int DisplayOrder { get; set; }
+    }
+}
